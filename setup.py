23a3350425ebac33b8a421d62af30a28ca857ae3
mgasmallah@hotmail.com <mgasmallah@hotmail.com>
2020-10-23 09:49:02 -0400

    modifying setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ import os, sys
 from distutils.core import setup
 from distutils.command.install import install as _install
 
-def _post_install(dir):
-    from subprocess import call
-    cwd = os.getcwd()
-    return
-
-
-class install(_install):
-    def run(self):
-        _install.run(self)
-        self.execute(_post_install, (self.install_lib,),
-                     msg="Running post install task")
+#def _post_install(dir):
+#    from subprocess import call
+#    cwd = os.getcwd()
+#    return
+#
+#
+#class install(_install):
+#    def run(self):
+#        _install.run(self)
+#        self.execute(_post_install, (self.install_lib,),
+#                     msg="Running post install task")
 
 
 
@@ -24,7 +24,6 @@ setup(
     description="A bridge between two languages",
     long_description="Allows for intercommunication between python and node environments",
     long_description_content_type="text/markdown",
-    cmdclass = {'install': install},
     packages=['bifrost'],
     install_requires=[
         'numpy',
