a989fbbf84f07dbf21756f4316f92bfeb7e8ed55
Chris M <cc.mcintyre@outlook.com>
2021-09-14 15:01:10 -0400

    reset to current package*.json, deleted build folder

--- build.off/Release/.deps/Release/obj.target/shmjs.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/obj.target/shmjs.node := g++ -shared -pthread -rdynamic -m64  -Wl,-soname=shmjs.node -o Release/obj.target/shmjs.node -Wl,--start-group Release/obj.target/shmjs/bifrost/shmjs.o -Wl,--end-group 
