f0f2430b943679fbc3a9674e79420ea9ad69a07e
Chris McIntyre <chris@kingsds.network>
2021-12-13 14:12:02 -0500

    Corrected redundant 'self' argument to class function.

--- bifrost/Job.py
+++ bifrost/Job.py
@@ -183,11 +183,11 @@ class Job:
             self.payment_account = payment_account
         if ( initial_slice_profile != False ):
             self.initial_slice_profile = initial_slice_profile     
-        self.results = self.__dcp_run(self)
+        self.results = self.__dcp_run()
 
     def local_exec(self, local_cores = 1):
         self.local_cores = local_cores
-        self.results = self.__dcp_run(self)
+        self.results = self.__dcp_run()
 
     def set_slice_payment_offer(self, slice_payment_offer):
         self.slice_payment_offer = slice_payment_offer
