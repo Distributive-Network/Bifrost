e400ee4c568269ddc1dd8a7834f93195cee42a6a
Chris McIntyre <chris@kingsds.network>
2022-03-22 02:49:46 -0400

    Fixed missing input definitions when pickle flags are set to false.

--- bifrost/dcp_compute_worker.py
+++ bifrost/dcp_compute_worker.py
@@ -6,11 +6,15 @@ if (pickle_arguments == True):
   # decode and unpickle secondary arguments to compute function
   parameters_decoded = codecs.decode( input_parameters.encode(), 'base64' )
   parameters_unpickled = cloudpickle.loads( parameters_decoded )
+else:
+  parameters_unpickled = input_parameters
 
 if (pickle_input == True):
   # decode and unpickle primary argument to compute function
   data_decoded = codecs.decode( input_data.encode(), 'base64' )
   data_unpickled = cloudpickle.loads( data_decoded )
+else:
+  data_unpickled = input_data
 
 if (pickle_function == True):
   # decode and unpickle compute_function from input cloudpickle object
