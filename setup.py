752c1851f96b531cfa6952055e6aba42eb0e3b55
Chris McIntyre <chris@kingsds.network>
2022-06-12 20:16:28 -0400

    Update version number and dependencies, build new wheel.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="Bifrost",
-    version="0.5.10",
+    version="0.5.11",
     author="Kings Distributed Systems",
     author_email="toolchains@kingsds.network",
     description="Python to JS intercommunication and execution",
