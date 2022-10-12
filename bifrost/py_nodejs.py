f818580039dd1d654c8abccd61a26f3e7c7dd98a
Chris McIntyre <9750327-ccmcintyre-kingsds@users.noreply.gitlab.com>
2022-10-12 18:16:55 -0400

    Remove NodeSTDProc from py_nodejs.py.

--- bifrost/py_nodejs.py
+++ bifrost/py_nodejs.py
@@ -8,11 +8,11 @@ import signal
 import time
 from pathlib import Path
 from subprocess import Popen, PIPE, check_output
-from threading import Thread, Event
 
 # local modules
 from .py_storage import VariableSync
 from .ReadWriteLock import ReadWriteLock
+from .NodeSTDProc import NodeSTDProc
 
 # PROGRAM
 
@@ -20,67 +20,6 @@ from .ReadWriteLock import ReadWriteLock
 NODE_LOCK       = ReadWriteLock()
 #variable that is being locked
 NODE_IS_RUNNING = False
-
-class NodeSTDProc(Thread):
-    '''
-    Helper class that is run in another thread from the main thread.
-    This runs in the background collecting information from the node
-    process and deciding how to manage it.
-
-    In particular, this manages stdout and completion of node process
-    execution.
-    '''
-    def __init__(self, process):
-        super(NodeSTDProc, self).__init__()
-        self.process     = process
-        self._stop_event = Event()
-        self.daemon      = True
-        self.start()
-
-    def stop(self):
-        '''
-        Stop this thread.
-        '''
-        self._stop_event.set()
-
-    def run(self):
-        '''Main loop for the thread.'''
-        while not self._stop_event.is_set():
-
-            global NODE_IS_RUNNING
-            global NODE_LOCK
-            output = self.process.stdout.readline().decode('utf-8')
-            #if process.poll() returns something,
-            #this means that the process has ended. End this thread too.
-            if self.process.poll() is not None:
-                NODE_LOCK.acquire_write()
-                NODE_IS_RUNNING = False
-                NODE_LOCK.release_write()
-                break
-            #if our output is an empty string, do nothing.
-            if output == '':
-                continue
-            #'if our output is not an empty string, try processing it.
-            if output:
-                try:
-                    #if our output is json serializeable, check if
-                    #it has type=== done before setting NODE_IS_RUNNING to false
-                    output_json = json.loads(output)
-                    if output_json['type'] == 'done':
-                        NODE_LOCK.acquire_write()
-                        NODE_IS_RUNNING = False
-                        NODE_LOCK.release_write()
-                    else:
-                        #otherwise print out the json
-                        if (output and len(output.strip()) > 0):
-                            print(output.strip())
-                    continue
-                except Exception as e:
-                    #otherwise print out the json
-                    if (output and len(output.strip()) > 0):
-                        print(output.strip())
-                    continue
-
 
 class Node():
     '''
