1538fc7418c5a8d944c2a1d96d0c0cb5ca81aee7
Chris McIntyre <9750327-ccmcintyre-kingsds@users.noreply.gitlab.com>
2022-10-13 21:22:47 -0400

    Update Work.py filenames for new submodule.

--- bifrost/dcp/Work.py
+++ bifrost/dcp/Work.py
@@ -5,9 +5,11 @@ from pathlib import Path
 
 # PROGRAM
 
-dcp_init_worker = Path(__file__).parent.joinpath('dcp_init_worker.py').read_text()
+dcp_init_worker = Path(__file__).parent.joinpath('js_work_setup.py').read_text()
 
-dcp_compute_worker = Path(__file__).parent.joinpath('dcp_compute_worker.py').read_text()
+dcp_compute_worker = Path(__file__).parent.joinpath('js_work_compute.py').read_text()
 
-js_work_function = Path(__file__).parent.joinpath('dcpWorkFunction.js').read_text()
+js_deploy_job = Path(__file__).parent.joinpath('pyJob.js').read_text()
+
+js_work_function = Path(__file__).parent.joinpath('pyWork.js').read_text()
 
